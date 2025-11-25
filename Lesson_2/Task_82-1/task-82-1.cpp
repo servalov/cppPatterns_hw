bfac102563847082b4a7179a4b7f1245d346728d
Sergey Valov <sergey.valov.eng@gmail.com>
2025-11-25 15:26:09 +0300

    Добавлено домашнее задание к занятию "Структурные шаблоны".

---
+++ Lesson_2/Task_82-1/task-82-1.cpp
@@ -0,0 +1,108 @@
+﻿// Структурные шаблоны: Proxy, Decorator, Adapter. Задание
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+class SimpleText
+{
+	public:
+		virtual void render(const std::string& text)
+		{
+			std::cout << text;
+		}
+};
+
+class DecoratedText :public SimpleText
+{
+	protected:
+		SimpleText* __simple_text;
+	public:
+		DecoratedText(SimpleText* simple_text) : __simple_text(simple_text) {}
+		
+};
+
+class ItalicText :public DecoratedText
+{
+	public:
+	ItalicText(SimpleText* simple_text) :DecoratedText(simple_text) {}
+	void render(const std::string& text) override
+	{
+		std::cout << "<i>";
+		__simple_text->render(text);
+		std::cout << "<i>";
+	}
+};
+
+class BoldText :public DecoratedText
+{
+	public:
+	BoldText(SimpleText* simple_text) :DecoratedText(simple_text) {}
+	void render(const std::string& text) override
+	{
+		std::cout << "<b>";
+		__simple_text->render(text);
+		std::cout << "<b>";
+	}
+};
+
+class Paragraph :public DecoratedText
+{
+	public:
+		Paragraph(SimpleText* simple_text) :DecoratedText(simple_text) {}
+	void render(const std::string& text) override
+	{
+		std::cout << "<p>";
+		__simple_text->render(text);
+		std::cout << "<p>";
+	}
+
+};
+
+class Reversed :public DecoratedText
+{
+	public:
+		Reversed(SimpleText* simple_text) :DecoratedText(simple_text) {}
+	void render(const std::string& text) override
+	{
+		std::string str{text};
+		std::reverse(str.begin(), str.end());
+		__simple_text->render(str);
+	}
+};
+
+class Link :public DecoratedText
+{
+	public:
+		Link(SimpleText* simple_text) :DecoratedText(simple_text) {}
+	void render(const std::string& text1, const std::string& text2)
+	{
+		std::cout <<"<a href=";
+		__simple_text->render(text1);
+		std::cout << ">";
+		__simple_text->render(text2);
+		std::cout << "</a>";
+	}
+};
+
+int main()
+{
+	setlocale(LC_ALL,"Russian");
+	std::cout << "Результаты рендеринга НTML:" << std::endl;
+	auto text = new ItalicText(new BoldText(new SimpleText()));
+	text->render(" Hello World ");
+
+	std::cout << std::endl;
+	auto text_block = new Paragraph(new SimpleText());
+	text_block->render("Hello world");
+	
+	std::cout << std::endl;
+	auto text_block2 = new Reversed(new SimpleText());
+	text_block2->render("Hello world");
+
+	std::cout << std::endl;
+	auto text_block3 = new Link(new SimpleText());
+	text_block3->render("netology.ru", "Hello world");
+	std::cout << std::endl;
+
+	return EXIT_SUCCESS;
+}
---
+++ Lesson_2/Task_82-2/task_82-2.cpp
@@ -0,0 +1,78 @@
+﻿#include <iostream>
+#include <string>
+#include <map>
+#include <memory>
+
+class VeryHeavyDatabase
+{
+	public:
+		virtual std::string get_data(const std::string& key)
+		{
+			return "Result of query: " + key;
+		}
+};
+
+class CacheDB :public VeryHeavyDatabase
+{
+	std::map<std::string, std::string> __cache;
+	std::unique_ptr<VeryHeavyDatabase> __real_db;
+
+	public:
+		CacheDB(std::unique_ptr<VeryHeavyDatabase> real_db) : __real_db(std::move(real_db)) {}
+		std::string get_data(const std::string& key) override
+		{
+			if (__cache.find(key) == __cache.end())
+			{
+				std::cout << "Get data from Original Data base" << std::endl;
+				__cache[key] = __real_db->get_data(key);
+			}
+			else
+			{
+				std::cout << "Get data from Cache Data base" << std::endl;
+			}
+			return __cache.at(key);
+		}
+};
+
+class TestDB :public VeryHeavyDatabase
+{
+	std::unique_ptr<VeryHeavyDatabase> __real_db;
+	public:
+		TestDB(std::unique_ptr<VeryHeavyDatabase> real_db) : __real_db(std::move(real_db)) {}
+		std::string get_data(const std::string& key) override
+		{
+			return "Test data";
+		}
+};
+
+class OneShotDB : public VeryHeavyDatabase
+{
+	std::unique_ptr<VeryHeavyDatabase> __real_db;
+	size_t limit_shots{}, count{};
+	public:
+		OneShotDB(std::unique_ptr<VeryHeavyDatabase> real_db, size_t shots) : __real_db(std::move(real_db)), limit_shots(shots) {}
+		std::string get_data(const std::string& key) 
+		{
+			++count;
+			return count <= limit_shots ? __real_db->get_data(key) : "Error. Limit of data query.";
+
+		}
+};
+
+int main()
+{
+	setlocale(LC_ALL,"Russian");
+	auto original_db = std::make_unique<VeryHeavyDatabase>();
+	auto limit_db = std::make_unique<OneShotDB>(std::move(original_db), 4);
+	std::cout << "\nРезультаты запросов в БД с лимитом обращений:" << std::endl;
+
+	std::cout << limit_db->get_data("key1") << std::endl;
+	std::cout << limit_db->get_data("key1") << std::endl;
+	std::cout << limit_db->get_data("key1") << std::endl;
+	std::cout << limit_db->get_data("key2") << std::endl;
+	std::cout << limit_db->get_data("key2") << std::endl;
+	std::cout << limit_db->get_data("key2") << std::endl;
+	std::cout << limit_db->get_data("key2") << std::endl;
+	
+	return EXIT_SUCCESS;
+}
